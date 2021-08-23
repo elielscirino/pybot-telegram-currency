514ebc8db6a4fe9f65c8d25c1296ae72f8441a84
elielscirino <elielscirino@gmail.com>
2021-08-23 03:15:44 -0300

    reorganize and add pound sterling currency

--- main.py
+++ main.py
@@ -9,17 +9,30 @@ def main():
 
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(CommandHandler('realdolar', realdolar))
+
+    #BRL
+    dp.add_handler(CommandHandler('realdollar', realdollar))
     dp.add_handler(CommandHandler('realeuro', realeuro))
-    dp.add_handler(CommandHandler('dolarreal', dolarreal))
-    dp.add_handler(CommandHandler('dolareuro', dolareuro))
+    dp.add_handler(CommandHandler('realpound', realpound))
+
+    #USD
+    dp.add_handler(CommandHandler('dollarreal', dollarreal))
+    dp.add_handler(CommandHandler('dollareuro', dollareuro))
+    dp.add_handler(CommandHandler('dollarpound', dollarpound))
+
+    #EUR
     dp.add_handler(CommandHandler('euroreal', euroreal))
-    dp.add_handler(CommandHandler('eurodolar', eurodolar))
+    dp.add_handler(CommandHandler('eurodollar', eurodollar))
+    dp.add_handler(CommandHandler('europound', europound))
+    
+    #GBP
+    dp.add_handler(CommandHandler('poundreal', poundreal))
+    dp.add_handler(CommandHandler('pounddollar', pounddollar))
+    dp.add_handler(CommandHandler('poundeuro', poundeuro))
+
 
     dp.add_error_handler(error)
 
     updater.start_polling()
     updater.idle()
 
-
-main()
